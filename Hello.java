3628248aa84d467a36725e29502abef4c5d49ec9
Almog Neger <Almog.Neger@opsys-sens.com>
2023-07-19 14:35:46 +0300

    Hello.java add by me

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=1; i<=10; i++)
-			System.out.println("diffrent thing..."+i);
+			System.out.println("how are you?..."+i);
 	}
 }
