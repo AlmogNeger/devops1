5644d4baf4919e7aae176b75c383db3743a6769b
Almog Neger <Almog.Neger@opsys-sens.com>
2023-07-19 18:00:20 +0300

    must04

--- Hello.java
+++ Hello.java
@@ -1,6 +1,6 @@
 public class Hello{
 	public static void main(String[] args){
 		for(int i=1; i<=10; i++)
-			System.out.println("i made changed03..."+i);
+			System.out.println("i made changed04..."+i);
 	}
 }
